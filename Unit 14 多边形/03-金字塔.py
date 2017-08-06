169b36faa051c1dd88ad6b49a3a0175338715ee1
某雪之 <xzgwz521@qq.com>
2017-08-06 16:00:24 +0800

    修改金字塔代码

--- Unit 14 多边形/03-金字塔.py
+++ Unit 14 多边形/03-金字塔.py
@@ -18,41 +18,38 @@ x2, y2, z2 = (x+20, y, z-20)
 x3, y3, z3 = (x+20, y, z+20)
 x4, y4, z4 = (x-20, y, z+20)
 
-points0=[]
+# 第一个三角形面
 points1=[]
+points1.append(minecraft.Vec3(x0, y0, z0))
+points1.append(minecraft.Vec3(x1, y1, z1))
+points1.append(minecraft.Vec3(x2, y2, z2))
+mcdrawing.drawFace(points1, True, block.WOOL.id, 2)
+
+# 第二个三角形面
 points2=[]
+points2.append(minecraft.Vec3(x0, y0, z0))
+points2.append(minecraft.Vec3(x2, y2, z2))
+points2.append(minecraft.Vec3(x3, y3, z3))
+mcdrawing.drawFace(points2, True, block.WOOL.id, 2)
+
+# 第三个三角形面
 points3=[]
+points3.append(minecraft.Vec3(x0, y0, z0))
+points3.append(minecraft.Vec3(x3, y3, z3))
+points3.append(minecraft.Vec3(x4, y4, z4))
+mcdrawing.drawFace(points3, True, block.WOOL.id, 2)
+
+# 第四个三角形面
 points4=[]
+points4.append(minecraft.Vec3(x0, y0, z0))
+points4.append(minecraft.Vec3(x4, y4, z4))
+points4.append(minecraft.Vec3(x1, y1, z1))
+mcdrawing.drawFace(points4, True, block.WOOL.id, 2)
 
 # 建造底面
+points0=[]
 points0.append(minecraft.Vec3(x1, y1, z1))
 points0.append(minecraft.Vec3(x2, y2, z2))
 points0.append(minecraft.Vec3(x3, y3, z3))
 points0.append(minecraft.Vec3(x4, y4, z4))
-
-# 第一个三角形面
-points1.append(minecraft.Vec3(x0, y0, z0))
-points1.append(minecraft.Vec3(x1, y1, z1))
-points1.append(minecraft.Vec3(x2, y2, z2))
-
-# 第二个三角形面
-points2.append(minecraft.Vec3(x0, y0, z0))
-points2.append(minecraft.Vec3(x2, y2, z2))
-points2.append(minecraft.Vec3(x3, y3, z3))
-
-# 第三个三角形面
-points3.append(minecraft.Vec3(x0, y0, z0))
-points3.append(minecraft.Vec3(x3, y3, z3))
-points3.append(minecraft.Vec3(x4, y4, z4))
-# 第四个三角形面
-points4.append(minecraft.Vec3(x0, y0, z0))
-points4.append(minecraft.Vec3(x4, y4, z4))
-points4.append(minecraft.Vec3(x1, y1, z1))
-
 mcdrawing.drawFace(points0, True, block.WOOL.id, 2)
-mcdrawing.drawFace(points1, True, block.WOOL.id, 2)
-mcdrawing.drawFace(points2, True, block.WOOL.id, 2)
-mcdrawing.drawFace(points3, True, block.WOOL.id, 2)
-mcdrawing.drawFace(points4, True, block.WOOL.id, 2)
-
-
